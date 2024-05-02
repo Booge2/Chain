04840259a17061558bfa633d090b0485de50eaa0
Booge2 <vladky54@gmail.com>
2024-05-02 21:07:38 +0300

    Completed

--- client.py
+++ client.py
@@ -1,25 +1,71 @@
 import socket
 
+
+# def main():
+#     host = "127.0.0.1"
+#     port = 12345
+#
+#     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#     client.connect((host, port))
+#
+#     while True:
+#         message = input("Введіть повідомлення: ")
+#         client.send(message.encode())
+#
+#         if message.strip().lower() == "exit":
+#             break
+#
+#         reply = client.recv(1024).decode()
+#         print(f"Сервер: {reply}")
+#
+#     client.close()
+#
+#
+# if __name__ == "__main__":
+#     main()
+# Завдання 2
+
+
+# def main():
+#     host = "127.0.0.1"
+#     port = 12345
+#
+#     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#     client_socket.connect((host, port))
+#
+#     country = input("Введіть країну: ")
+#     city = input("Введіть місто: ")
+#
+#     client_socket.send(f"{city},{country}".encode())
+#
+#     response = client_socket.recv(1024).decode()
+#     print(response)
+#
+#     client_socket.close()
+#
+#
+# if __name__ == "__main__":
+#     main()
+# Завдання 3
+
+
 def main():
     host = "127.0.0.1"
     port = 12345
 
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect((host, port))
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((host, port))
 
-    while True:
-        message = input("Введіть повідомлення: ")
-        client.send(message.encode())
+    text = input("Введіть текст для перекладу: ")
+    target_language = input("Введіть код цільової мови: ")
 
-        if message.strip().lower() == "exit":
-            break
+    client_socket.send(f"{text},{target_language}".encode())
 
-        reply = client.recv(1024).decode()
-        print(f"Сервер: {reply}")
+    translated_text = client_socket.recv(1024).decode()
+    print("Переклад:", translated_text)
 
+    client_socket.close()
 
-
-    client.close()
 
 if __name__ == "__main__":
     main()
--- main.py
+++ main.py
@@ -1,4 +1,90 @@
 import socket
+import threading
+from googletrans import Translator
+
+
+# def main():
+#     host = "127.0.0.1"
+#     port = 12345
+#
+#     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#     server_socket.bind((host, port))
+#     server_socket.listen(1)
+#
+#     print("Сервер запущено...")
+#
+#     while True:
+#         client, addr = server_socket.accept()
+#         print(f"З’єднано з {addr}")
+#
+#         while True:
+#             message = client.recv(1024).decode()
+#             print(f"Клієнт: {message}")
+#
+#             if message.strip().lower() == "exit":
+#                 break
+#
+#             reply = input("Відповідь: ")
+#             client.send(reply.encode())
+#
+#         client.close()
+#
+#
+# if __name__ == "__main__":
+#     main()
+# Завдання 2
+
+
+# def handle_client(client_socket):
+#     request = client_socket.recv(1024).decode()
+#
+#     response = "Погода на тиждень для міста {}:\n Monday: 20°C, Tuesday: 22°C, Wednesday: 23°C, Thursday: 21°C, Friday: 19°C, Saturday: 18°C, Sunday: 20°C".format(
+#         request)
+#     client_socket.send(response.encode())
+#
+#     client_socket.close()
+#
+#
+# def main():
+#     host = "127.0.0.1"
+#     port = 12345
+#
+#     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#     server_socket.bind((host, port))
+#     server_socket.listen(5)
+#
+#     print(f"Сервер запущено на {host}:{port}")
+#
+#     while True:
+#         client_socket, addr = server_socket.accept()
+#         print(f"З'єднання від {addr[0]}:{addr[1]}")
+#
+#         client_handler = threading.Thread(target=handle_client, args=(client_socket,))
+#         client_handler.start()
+#
+#
+# if __name__ == "__main__":
+#     main()
+# Завдання 3
+
+
+def translate_text(text, target_language):
+    translator = Translator()
+    translation = translator.translate(text, dest=target_language)
+    return translation.text
+
+
+def handle_client(client_socket):
+    request = client_socket.recv(1024).decode()
+    print("Запит на переклад:", request)
+
+    text, target_language = request.split(",")
+
+    translated_text = translate_text(text, target_language)
+
+    client_socket.send(translated_text.encode())
+
+    client_socket.close()
 
 
 def main():
@@ -7,28 +93,15 @@ def main():
 
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((host, port))
-    server_socket.listen(1)
+    server_socket.listen(5)
 
-    print("Сервер запущено...")
+    print(f"Сервер запущено на {host}:{port}")
 
     while True:
-        client, addr = server_socket.accept()
-        print(f"З’єднано з {addr}")
+        client_socket, addr = server_socket.accept()
+        print(f"З'єднання від {addr[0]}:{addr[1]}")
 
-        while True:
-            message = client.recv(1024).decode()
-            print(f"Клієнт: {message}")
-
-            if message.strip().lower() == "exit":
-                break
-
-
-            reply = input("Відповідь: ")
-            client.send(reply.encode())
-
-
-
-        client.close()
+        handle_client(client_socket)
 
 
 if __name__ == "__main__":
